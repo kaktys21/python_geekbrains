e3d8008e09991af83d92e3cfab73db70bd7a0ae6
kaktys21 <epishegor@yandex.ru>
2020-12-16 17:29:32 +0300

    Первая домашка

---
+++ 11.py
@@ -0,0 +1,7 @@
+'''Поработайте с переменными, создайте несколько, выведите на экран, запросите у пользователя несколько чисел и строк и сохраните в переменные, выведите на экран.'''
+
+a = input('Enter str: ')
+b = int(input('Enter num: '))
+c = int(input('Enter another num: '))
+
+print('Your line: {}\nYpur number: {}\nYour second number: {}'.format(a,b,c))
