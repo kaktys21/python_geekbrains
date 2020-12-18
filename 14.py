a945975ce767f6275ca55529717c61d1d2ccb916
kaktys21 <76106796+kaktys21@users.noreply.github.com>
2020-12-18 20:11:10 +0300

    Update 14.py

--- 14.py
+++ 14.py
@@ -1,5 +1,6 @@
 '''Пользователь вводит целое положительное число. Найдите самую большую цифру в числе. Для решения используйте цикл while и арифметические операции.'''
 
+'''через строки'''
 num = input('Enter num: ')
 maxNum = num[0]
 i = 1
@@ -8,4 +9,14 @@ while i < len(num):
         maxNum = num[i]
     i += 1
     
+print(maxNum)
+
+'''арифметика'''
+num = int(input('Enter num: '))
+maxNum = -1
+while num > 10:
+    CurNum = num % 10
+    num //= 10
+    if CurNum > maxNum:
+        maxNum = CurNum
 print(maxNum)
