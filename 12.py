92fb9a84f5bed8dfb569179ae47fe80d99a1a912
Egor Epishev <76106796+kaktys21@users.noreply.github.com>
2020-12-24 22:59:28 +0300

    Update 12.py

--- 12.py
+++ 12.py
@@ -1,8 +1,5 @@
 '''Пользователь вводит время в секундах. Переведите время в часы, минуты и секунды и выведите в формате чч:мм:сс. Используйте форматирование строк.'''
 
-def TwoDigits(num):
-    return str(num) if num >= 10 else '0' + str(num)
-
 seconds = int(input("Seconds: "))
 hours = seconds // 3600
 minutes = (seconds - hours * 3600) // 60
@@ -11,4 +8,4 @@ seconds = seconds - (hours * 3600 + minutes * 60)
 if hours > 24:
     hours = 24
 
-print('{}:{}:{}'.format(TwoDigits(hours),TwoDigits(minutes),TwoDigits(seconds)))
+print('{:02}:{:02}:{:02}'.format(hours, minutes, seconds))
